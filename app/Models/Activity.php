b00c604d1027717d19e09e17fa70325a3fb088fb
aryapanangsang <farizrizal29@gmail.com>
2025-02-27 16:22:29 +0700

    First Commit - Task Management

---
+++ app/Models/Activity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Status;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Activity extends Model
+{
+    use HasFactory;
+    protected $guarded = [ 'id' ];
+
+    public function status()
+    {
+        return $this->hasMany(Status::class);
+    }
+}
